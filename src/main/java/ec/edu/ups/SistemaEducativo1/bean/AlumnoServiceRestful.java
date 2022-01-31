e57499b6b603884025b922a40c35bfb85f87842c
Draaft4 <draaft@outlook.es>
2022-01-31 10:06:34 -0500

    Creacion del Servicio de Solicitudes

--- src/main/java/ec/edu/ups/SistemaEducativo1/bean/AlumnoServiceRestful.java
+++ src/main/java/ec/edu/ups/SistemaEducativo1/bean/AlumnoServiceRestful.java
@@ -14,17 +14,22 @@ import javax.ws.rs.core.MediaType;
 
 import ec.edu.ups.SistemaEducativo1.bussiness.AlumnoONLocal;
 import ec.edu.ups.SistemaEducativo1.bussiness.CalificacionesONLocal;
+import ec.edu.ups.SistemaEducativo1.bussiness.SolicitudesONLocal;
 import ec.edu.ups.SistemaEducativo1.model.Alumno;
 import ec.edu.ups.SistemaEducativo1.model.Asignatura;
 import ec.edu.ups.SistemaEducativo1.model.Calificaciones;
+import ec.edu.ups.SistemaEducativo1.model.Solicitudes;
 
 @Path("alumno")
 public class AlumnoServiceRestful {
-	
+
 	@Inject
 	private AlumnoONLocal dao;
 	@Inject
 	private CalificacionesONLocal daoCalifiaciones;
+	@Inject
+	private SolicitudesONLocal daoSolicitudes;
+
 	@PUT
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
@@ -36,31 +41,32 @@ public class AlumnoServiceRestful {
 		}
 		return null;
 	}
-	
+
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public List<Alumno> listarContactos(){
+	public List<Alumno> listarContactos() {
 		return dao.getAlumnos();
 	}
-	
-	
+
 	@PUT
 	@Path("calificaciones")
 	@Produces(MediaType.APPLICATION_JSON)
-	public String agregarCalificacion(@QueryParam("cedula")String cedulaEstudiante, @QueryParam("asignatura") int codAsignatura, @QueryParam("calificacion")int nota) {
+	public String agregarCalificacion(@QueryParam("cedula") String cedulaEstudiante,
+			@QueryParam("asignatura") int codAsignatura, @QueryParam("calificacion") int nota) {
 		Calificaciones calificacion = new Calificaciones();
-		calificacion.setNotaParcial(nota);
-		try {
-			daoCalifiaciones.insertarCalificaciones(calificacion);
-		} catch (Exception e1) {
-		}
 		Alumno alumno = dao.obtenerAlumno(cedulaEstudiante);
-		if(alumno!=null) {
-			for(Asignatura asignatura:alumno.getAsignaturas()) {
-				if(asignatura.getCodigo()==codAsignatura) {
-					if(asignatura.getCalificaciones()!=null) {
+		if (alumno != null) {
+			calificacion.setNotaParcial(nota);
+			try {
+				daoCalifiaciones.insertarCalificaciones(calificacion);
+			} catch (Exception e1) {
+			}
+
+			for (Asignatura asignatura : alumno.getAsignaturas()) {
+				if (asignatura.getCodigo() == codAsignatura) {
+					if (asignatura.getCalificaciones() != null) {
 						asignatura.getCalificaciones().add(calificacion);
-					}else {
+					} else {
 						asignatura.setCalificaciones(new ArrayList<Calificaciones>());
 						asignatura.getCalificaciones().add(calificacion);
 					}
@@ -72,9 +78,40 @@ public class AlumnoServiceRestful {
 				return e.getMessage();
 			}
 			return "";
-		}else {
+		} else {
 			return "No se ha encontrado el alumno";
 		}
-}
-	
+	}
+
+	@PUT
+	@Path("solicitudes")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String agregarSolicitud(@QueryParam("cedula") String cedulaEstudiante,
+			@QueryParam("contenido") String contenido) {
+
+		Alumno alumno = dao.obtenerAlumno(cedulaEstudiante);
+		if (alumno != null) {
+			Solicitudes solicitud = new Solicitudes();
+			solicitud.setContenido(contenido);
+			try {
+				daoSolicitudes.crearSolicitud(solicitud);
+			} catch (Exception e) {
+			}
+			if (alumno.getSolicitudes() != null) {
+				alumno.getSolicitudes().add(solicitud);
+			} else {
+				alumno.setSolicitudes(new ArrayList<Solicitudes>());
+				alumno.getSolicitudes().add(solicitud);
+			}
+			try {
+				dao.actualiarAlumno(alumno);
+			} catch (Exception e) {
+				return e.getMessage();
+			}
+			return "";
+		} else {
+			return "No se ha encontrado el alumno";
+		}
+	}
+
 }
